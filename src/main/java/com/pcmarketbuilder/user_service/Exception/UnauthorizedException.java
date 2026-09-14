d054fc1cecdc5bb03248c90dda9c75db07ccda14
sotom28 <rod.sotod@duocuc.cl>
2026-09-14 18:33:56 -0300

    subirlo ecr 1 microservico usuario 1

--- src/main/java/com/pcmarketbuilder/user_service/Exception/UnauthorizedException.java
+++ src/main/java/com/pcmarketbuilder/user_service/Exception/UnauthorizedException.java
@@ -1,5 +1,6 @@
 package com.pcmarketbuilder.user_service.Exception;
 
+
 public class UnauthorizedException extends RuntimeException {
     public UnauthorizedException(String message) {
         super(message);
